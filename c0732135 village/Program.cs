bbd9c1fbe083bc9474b15491b9d71f7748392a53
visualstudio <visualstudio@122-PC08>
2019-02-01 19:51:06 -0500

    commit

---
+++ c0732135 village/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c0732135_village
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
